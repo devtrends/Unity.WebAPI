f0924da1c40e4a53d04cfd85891078b11af953d9
Paul Hiles <register.devtrends@ubermail.co.uk>
2019-07-05 11:53:46 -0400

    updated dependencies to fix Unity reference issue

--- Unity.WebApi/Properties/AssemblyInfo.cs
+++ Unity.WebApi/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("06fd1655-583e-4b28-bbe5-fdded9b9f718")]
-[assembly: AssemblyVersion("5.3.0")]
-[assembly: AssemblyFileVersion("5.3.0")]
+[assembly: AssemblyVersion("5.4.0")]
+[assembly: AssemblyFileVersion("5.4.0")]
